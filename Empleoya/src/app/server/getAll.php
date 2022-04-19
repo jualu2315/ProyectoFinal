61fcbcafc45c970e11f87b29f5b406aff50383a9
GRJhubanRodriguez <GRJhubanRodriguez@users.noreply.github.com>
2022-04-18 19:02:52 -0500

    Cambio de ruta a registro

--- Empleoya/src/app/server/getAll.php
+++
@@ -1,7 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: http://localhost:4200");
-$bd = include_once "bd.php";
-$sentencia = $bd->query("SELECT id, nombre, identificacion, profesion, edad, telefono, correo, perfil, aspiracion, experiencia, nivel, referencias FROM candidatos");
-$candidato = $sentencia->fetchAll(PDO::FETCH_OBJ);
-echo json_encode($candidato);
-?>
